57270fc05cb8fe24df1c5a2c41292631e3d4a989
peng522642110@icloud.com <peng522642110@icloud.com>
2018-06-05 22:16:00 +0800

    高级搜索前端完成

---
+++ pages/car/advanced_search/advanced_search.js
@@ -0,0 +1,187 @@
+// pages/car/advanced_search/advanced_search.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    level:[
+      { name: "轿车",value:'0',isCheck:false},
+      { name: "SUV", value: '1', isCheck: true },
+      { name: "MPV", value: '2', isCheck: false },
+      { name: "跑车", value: '3', isCheck: false },
+      { name: "微面", value: '4', isCheck: false },
+      { name: "微卡", value: '5', isCheck: false },
+      { name: "轻客", value: '6', isCheck: false },
+      { name: "皮卡", value: '7', isCheck: false },
+    ],
+    structure:[
+      { name: "两厢", value: '0', isCheck: false },
+      { name: "三厢", value: '1', isCheck: false },
+      { name: "掀背", value: '2', isCheck: false },
+      { name: "旅行版", value: '3', isCheck: false },
+      { name: "硬顶敞篷车", value: '4', isCheck: false },
+      { name: "软顶敞篷车", value: '5', isCheck: false },
+      { name: "货车", value: '6', isCheck: false },
+      { name: "客车", value: '7', isCheck: false },
+    ],
+    displacement:[
+      { name: "1.0L及以下", value: '0', isCheck: false },
+      { name: "1.1-1.6L", value: '1', isCheck: false },
+      { name: "1.7-2.0L", value: '2', isCheck: false },
+      { name: "2.1-2.5L", value: '3', isCheck: false },
+      { name: "2.6-3.0L", value: '4', isCheck: false },
+      { name: "2.1-4.0L", value: '5', isCheck: false },
+      { name: "4.0L以上", value: '6', isCheck: false },
+    ],
+    Transmission:[
+      { name: "手动", value: '0', isCheck: false },
+      { name: "自动", value: '1', isCheck: false },
+    ],
+    city:[
+      { name: "中国", value: '0', isCheck: false },
+      { name: "德国", value: '1', isCheck: false },
+      { name: "日本", value: '2', isCheck: false },
+      { name: "美国", value: '3', isCheck: false },
+      { name: "韩国", value: '4', isCheck: false },
+      { name: "法国", value: '5', isCheck: false },
+      { name: "英国", value: '6', isCheck: false },
+      { name: "其他", value: '7', isCheck: false },
+    ],
+    attribute:[
+      { name: "国产", value: '0', isCheck: false },
+      { name: "进口", value: '1', isCheck: false },
+    ],
+    seat:[
+      { name: "2座", value: '0', isCheck: false },
+      { name: "4座", value: '1', isCheck: false },
+      { name: "5座", value: '2', isCheck: false },
+      { name: "6座", value: '3', isCheck: false },
+      { name: "7座", value: '4', isCheck: false },
+      { name: "7以上", value: '5', isCheck: false },
+    ],
+    energy:[
+      { name: "汽油", value: '0', isCheck: false },
+      { name: "柴油", value: '1', isCheck: false },
+      { name: "油电混合", value: '2', isCheck: false },
+      { name: "纯电动", value: '3', isCheck: false },
+    ],
+    driving_method:[
+      { name: "前驱", value: '0', isCheck: false },
+      { name: "后驱", value: '1', isCheck: false },
+      { name: "四驱", value: '2', isCheck: false },
+    ],
+    price:[0,0],
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  },
+  formSubmit:function(e){
+    console.log(e)
+    var param = { titles: ['col1', 'col2', 'col3'],price:[1,2] }
+    var that = this
+
+    wx.request({
+      method: 'POST',
+      dataType: "json",
+      url: 'http://localhost:8762/api-basicS/search/advancedSearch',
+      data:e.detail.value,
+      header:{
+        "Accept": "application/json, text/javascript, */*; q=0.01",
+        "Content-Type":"application/x-www-form-urlencoded; charset=UTF-8"
+      },
+      success: function (res) {
+        
+      }
+    })
+  },
+  serviceValChange: function (e) {
+    console.log(e)
+    var that = this
+    var id = e.currentTarget.id
+    var array
+    if(id=="level"){
+      array = that.data.level;
+    } else if (id =="structure"){
+      array = that.data.structure;
+    } else if (id =="displacement"){
+      array = that.data.displacement
+    } else if (id =="Transmission"){
+      array = that.data.Transmission
+    } else if (id =="city"){
+      array = that.data.city
+    } else if (id =="attribute"){
+      array = that.data.attribute
+    } else if (id =="seat"){
+      array = that.data.seat
+    } else if (id =="energy"){
+      array = that.data.energy
+    } else if (id =="driving_method"){
+      array = that.data.driving_method
+    }
+    var checkArr = e.detail.value;
+    for (var i = 0; i < array.length; i++) {
+      if (checkArr.indexOf(i + "") != -1) {
+        array[i].isCheck = true;
+      } else {
+        array[i].isCheck = false;
+      }
+    }
+    that.setData({
+      [id]: array
+    })
+  }  
+})
--- pages/car/cars.js
+++ pages/car/cars.js
@@ -1,11 +1,20 @@
-// pages/car/cars.js
+//先引用城市数据文件
+var city = require('../../utils/test/test.js')
+var lineHeight = 0;
+var endWords = "";
+var isNum;
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    price: [{ from: 0, to: 5, text: "5万以下" }, { from: 0, to: 5, text: "5万以下" }, { from: 0, to: 5, text: "5万以下" }, { from: 0, to: 5, text: "5万以下" }, { from: 0, to: 5, text: "5万以下" }, { from: 0, to: 5, text: "5万以下" }, { from: 0, to: 5, text: "5万以下" }, { from: 0, to: 5, text: "5万以下" }],
+    brand: [{ brandName: "123", src: "/resource/image/car-pic/pic2.jpg" }, { brandName: "123", src: "/resource/image/car-pic/pic2.jpg" }, { brandName: "123", src: "/resource/image/car-pic/pic2.jpg" }, { brandName: "123", src: "/resource/image/car-pic/pic2.jpg" }, { brandName: "123", src: "/resource/image/car-pic/pic2.jpg" }, { brandName: "123", src: "/resource/image/car-pic/pic2.jpg" }, { brandName: "123", src: "/resource/image/car-pic/pic2.jpg" }, { brandName: "123", src: "/resource/image/car-pic/pic2.jpg" }, { brandName: "123", src: "/resource/image/car-pic/pic2.jpg" }, { brandName: "123", src: "/resource/image/car-pic/pic2.jpg" }],
+
+    hidden: 0,
+    cityName: "", //获取选中的城市名
+
   },
 
   /**
@@ -19,7 +28,20 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+    // 生命周期函数--监听页面初次渲染完成
+    var cityChild = city.City[0];
+    var that = this;
+    wx.getSystemInfo({
+      success: function (res) {
+        lineHeight = (res.windowHeight - 100) / 22;
+        console.log(res.windowHeight - 100)
+        that.setData({
+          city: cityChild,
+          winHeight: res.windowHeight,
+          lineHeight: lineHeight
+        })
+      }
+    })
   },
 
   /**
@@ -62,5 +84,93 @@ Page({
    */
   onShareAppMessage: function () {
   
+  },
+  advancedSearch:function(e){
+    wx.navigateTo({
+      url: '/pages/car/advanced_search/advanced_search',
+    })
+  },
+
+  // 商标列表
+  //触发全部开始选择
+  chStart: function () {
+    this.setData({
+      trans: "0.3",
+      // hidden: false
+    })
+  },
+  //触发结束选择
+  chEnd: function () {
+    this.setData({
+      trans: "0",
+      // hidden: true,
+      scrollTopId: this.endWords
+    })
+  },
+  //获取文字信息
+  getWords: function (e) {
+    var id = e.target.id;
+    this.endWords = id;
+    isNum = id;
+    this.setData({
+      showwords: this.endWords
+    })
+  },
+  //设置文字信息
+  setWords: function (e) {
+    var id = e.target.id;
+    this.setData({
+      scrollTopId: id
+    })
+  },
+  // 滑动选择城市
+  chMove: function (e) {
+    console.log(e)
+    var y = e.touches[0].clientY;
+    var offsettop = e.currentTarget.offsetTop;
+    var height = 0;
+    var that = this;
+    ;
+    var cityarr = ["A", "B", "C", "D", "E", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "W", "X", "Y", "Z"]
+    // 获取y轴最大值
+    wx.getSystemInfo({
+      success: function (res) {
+        
+        height = res.windowHeight - 10;
+      }
+    });
+    //判断选择区域,只有在选择区才会生效
+    if (y > offsettop && y < height) {
+      // console.log((y-offsettop)/lineHeight)
+      var num = parseInt((y - offsettop) / lineHeight);
+      endWords = cityarr[num];
+      // 这里 把endWords 绑定到this 上，是为了手指离开事件获取值
+      that.endWords = endWords;
+    };
+    //去除重复，为了防止每次移动都赋值 ,这里限制值有变化后才会有赋值操作，
+    //DOTO 这里暂时还有问题，还是比较卡，待优化
+    if (isNum != num) {
+      // console.log(isNum);
+      isNum = num;
+      that.setData({
+        showwords: that.endWords,
+        scrollTopId: that.endWords
+      })
+    }
+  },
+  //选择城市，并让选中的值显示在文本框里
+  bindCity: function (e) {
+    console.log(e);
+    var cityName = e.currentTarget.dataset.city;
+    this.setData({ cityName: cityName })
+  },
+  //滑动条显示事件
+  scroll:function(e){
+    var that = this
+    console.log(e.detail.scrollTop)
+    that.setData({
+      hidden: e.detail.scrollTop
+    })
+    
   }
 })
