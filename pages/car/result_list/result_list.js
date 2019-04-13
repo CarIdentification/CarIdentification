0184c2bcbd92bc1f00b1574e3b8f5989a8e4f902
Morgan <dmc150101@gmail.com>
2019-04-13 23:09:26 +0800

    1. shop info update

--- pages/car/result_list/result_list.js
+++ pages/car/result_list/result_list.js
@@ -6,30 +6,29 @@ Page({
    * 页面的初始数据
    */
   data: {
-    cars:[],
+    cars: [],
+    mark: 0,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     var that = this
+    var mark = options.mark
     wx.request({
       method: 'POST',
       dataType: "json",
-      url: app.globalData.localhost +'/api-basicS/search/advancedSearch',
-      data: 
-
-        app.globalData.advanced_mess
-        
-        ,
+      url: app.globalData.localhost + '/api-basicS/search/advancedSearch',
+      data: app.globalData.advanced_mess,
       header: {
         "Accept": "application/json, text/javascript, */*; q=0.01",
         "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"
       },
-      success: function (res) {
+      success: function(res) {
         that.setData({
-          cars:res.data.entity
+          cars: res.data.entity,
+          mark: mark
         })
       }
     })
@@ -38,52 +37,52 @@ Page({
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-  
+  onReady: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-  
+  onShow: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-  
+  onHide: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-  
+  onUnload: function() {
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-  
+  onPullDownRefresh: function() {
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-  
+  onReachBottom: function() {
+
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-  
+  onShareAppMessage: function() {
+
   },
-  showCarMess: function(e){
+  showCarMess: function(e) {
     wx.navigateTo({
       url: '../result_car/result_car?id=' + e.currentTarget.id,
     })
